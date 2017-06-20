baec9ca65abd640a18aa513507051c4703fe4d04
tuckrr <christuckolsen@gmail.com>
2017-06-20 18:52:10 -0400

    added map.newMap

--- map.lua
+++ map.lua
@@ -3,15 +3,34 @@
 -- also contains tile definitions because they only get used in map
 
 m = {}
-mft = {}
-tft = {}
+local mft = {}
+local tft = {}
 
+function m.newMap(widthP, heightP, tileWidthP, tileHeightP)
+  tempMap = {}
+  for i = 1, widthP do
+    tempMap[i] = {}
+  end
+  return setmetatable({
+    width = widthP, height = heightP,
+    widthPx = widthP * tileWidthP,
+    heightPx = heightP * tileHeightP,
+    tileWidth = tileWidthP,
+    tileHeight = tileHeightP,
+    streaming = false, -- level streaming/chunk based system. tbd exactsies
+    tilesets = {},
+    map = tempMap
+  }, {__index = mft})
+end
+
+function mft:setLocation(xP, yP)
+  -- handle entities, tile, and more -.-
+end
 --[[
 todo:
 primary functionality
-  m.newMap
-  mft.setTile
-  mft.getTile
+  mft.setLocation
+  mft.getLocation
   mft.init
   mft.update
   mft.draw
@@ -19,8 +38,8 @@ primary functionality
   tft.addTile
 
 secondary
-  mft.loadFile
-  mft.writeFile
+  mft.loadFile (the real meat)
+  mft.writeFile (for editing)
   tft.addTileBatch
   tft.addAniTile
   tft.addAniTileBatch
