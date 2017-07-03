97e5f1887556385a2d3646761dbbd5495e1cefa7
tuckrr <tuck@Christophers-MacBook-Pro-4.local>
2017-07-02 23:52:03 -0400

    track fps

--- ui.lua
+++ ui.lua
@@ -1,6 +1,22 @@
 -- koi.ui
--- handles the ui/heads up/etc on a panel
+-- handles the ui/heads up/etc on a panel and otherwise
 
 ui = {}
+uift = {}
+
+function ui.newUI(fpsP)
+  return setmetatable({
+    drawFPS = fpsP
+  }, {__index = uift})
+end
+
+function uift:update(dt)
+end
+
+function uift:draw()
+  if self.drawFPS then
+    love.graphics.print(string.format('%d', love.timer.getFPS()), 16, 16)
+  end
+end
 
 return ui
