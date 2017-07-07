718c0466fa0a1ef5466c200c7b0e4dc6fc18df99
tuckrr <tuck@Christophers-MacBook-Pro-4.local>
2017-07-07 01:15:40 -0400

    added non-looping option

--- sprite.lua
+++ sprite.lua
@@ -11,7 +11,7 @@
 s = {}
 local sft = {} -- sprite function table
 
-function s.newSprite(imgP, wP, hP, animSpeedP)
+function s.newSprite(imgP, wP, hP, animSpeedP, defaultAnimP)
 	return setmetatable({
 		spriteSheet = love.graphics.newImage(imgP),
 		r = 0,
@@ -20,10 +20,11 @@ function s.newSprite(imgP, wP, hP, animSpeedP)
 		kx = 0, ky = 0,
 		width = wP or 32, height = hP or 32,
 		animSpeed = animSpeedP, -- or nil, no need to be explicit
-		currentAnimation = "",
+		currentAnimation = defaultAnimP or '',
 		animations = {},
 		elapsedTime = 0,
-		currentFrame = 1
+		currentFrame = 1,
+		loop = true
 	}, {__index = sft})
 end
 
@@ -39,13 +40,27 @@ function sft:addFrame(anim, x, y)
 	return #self.animations[anim]
 end
 
+function sft:setCurrentAnimation(anim, loop)
+	if not (self.currentAnimation == anim) then
+		if loop or loop == nil then
+			self.loop = true
+		else
+			self.loop = false
+		end
+		self.currentAnimation = anim
+		self.currentFrame = 1
+	end
+end
+
 function sft:update(dt)
 	if self.animSpeed and #self.animations[self.currentAnimation] > 0 then -- animSpeed = nil means static
 		self.elapsedTime = self.elapsedTime + dt
 		if self.elapsedTime >= self.animSpeed then
 			self.elapsedTime = 0
 			if self.currentFrame >= #self.animations[self.currentAnimation] then
-				self.currentFrame = 1
+				if self.loop then
+					self.currentFrame = 1
+				end
 			else
 				self.currentFrame = self.currentFrame + 1
 			end
